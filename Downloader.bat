70fb4949ad79d7166f447f38cacd3f8342c4802f
CodeFoxy <96522178+CodeFoxy-Github@users.noreply.github.com>
2022-07-05 19:05:33 +0800

    Update Downloader.bat

--- Downloader.bat
+++ Downloader.bat
@@ -1,10 +1,11 @@
 @echo off
-<<<<<<< HEAD
 echo 1.Bilibili
 echo 2.Youtube
-echo 3.Facebook
+echo 3.Faceboo
+echo 4.Twitter
 set /p ans="App Name? "
-if %ans% == 1 (
+set /p id="Video Link?"
+bitsadmin /transfer mydownloadjob /download /priority FOREGROUND "https://github.com/ytdl-org/youtube-dl/releases/download/2021.12.17/youtube-dl.exe" "%cd%\youtube-dl.exe"
+youtube-dl %id%
 
-bitsadmin /transfer mydownloadjob /download /priority FOREGROUND "https://github.com/ytdl-org/youtube-dl/releases/download/2021.12.17/youtube-dl.exe" "%cd%\Discord.zip"
-)
+
