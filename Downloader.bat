729982e0b1a96a24a41f35e6f4091d5aa7c0969c
CodeFoxy <96522178+CodeFoxy-Github@users.noreply.github.com>
2022-07-05 19:06:02 +0800

    Update Downloader.bat

--- Downloader.bat
+++ Downloader.bat
@@ -4,7 +4,9 @@ echo 2.Youtube
 echo 3.Faceboo
 echo 4.Twitter
 set /p ans="App Name? "
+cls
 set /p id="Video Link?"
+cls
 bitsadmin /transfer mydownloadjob /download /priority FOREGROUND "https://github.com/ytdl-org/youtube-dl/releases/download/2021.12.17/youtube-dl.exe" "%cd%\youtube-dl.exe"
 youtube-dl %id%
 
