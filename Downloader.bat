a4fc224f2a5797a299a20172ccb1c8b9f6fef50d
CodeFoxy <96522178+CodeFoxy-Github@users.noreply.github.com>
2022-07-05 18:42:20 +0800

    Update Downloader.bat

--- Downloader.bat
+++ Downloader.bat
@@ -8,10 +8,3 @@ if %ans% == 1 (
 
 bitsadmin /transfer mydownloadjob /download /priority FOREGROUND "https://github.com/ytdl-org/youtube-dl/releases/download/2021.12.17/youtube-dl.exe" "%cd%\Discord.zip"
 )
-=======
-echo 1.Bilibili
-echo 2.Youtube
-echo 3.Facebook
-set /p ans="App Name? "
-
->>>>>>> ab51bf9b3d4a56bc690a3bd6ca308e44502c9f6f
